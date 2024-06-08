e0489c947e1a86de40bf2be8f9a9354e75b36722
Alexandre Sevilha <alexandre.sevilha1@gmail.com>
2024-06-08 11:36:46 -0300

    Modificações nas paginas e criado tela de login

---
+++ SistemaGeral/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaGeral.Models;
+
+namespace SistemaGeral.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            Funcionario usuario = new Funcionario();
+
+            return View("Index", usuario);
+        }
+
+        [HttpPost]
+        public IActionResult Login(Funcionario usuario)
+        {
+            usuario.Email = "Funcionou";
+            return View("Index", usuario);
+        }
+    }
+}
--- SistemaGeral/Program.cs
+++ SistemaGeral/Program.cs
@@ -47,6 +47,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Login}/{action=Index}/{id?}");
 
 app.Run();
